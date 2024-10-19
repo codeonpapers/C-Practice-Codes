132e8e84a484f5ffda28649da5659146aa92f631
swamirakesh <rswami5579@fmail.com>
2024-10-19 18:04:17 +0530

    Uploaded

---
+++ Array_Element_Swapping.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int size=5;
+int main()
+{
+    int arr1[size]={1,2,3,4,5};
+    int arr2[size]={6,7,8,9,10};
+    int temp1;
+    int temp2;
+    for(int i=0;i<(sizeof(arr1)/sizeof(arr1[0]));i++)
+    {
+        temp1=arr1[i];
+        temp2=arr2[i];
+        arr2[i]=temp1;
+        arr1[i]=temp2;
+    }
+    //Print swapped elements.
+    printf("First Array: ");
+    for(int i=0;i<(sizeof(arr1)/sizeof(arr1[0]));i++)
+    {
+        printf("%d\t",arr2[i]);
+    }
+    return 0;
+}
